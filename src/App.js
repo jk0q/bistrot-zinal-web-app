e00840bd663d4d1617e5b018702525b55860bb10
Votre Nom <votre@email.com>
2025-03-12 20:03:48 +0100

    Correction de l'extension des fichiers JSX et de la configuration Vite

--- src/App.js
+++
@@ -1,1 +0,0 @@
-import React from 'react'; export default function App() { return ( <div> <h1>Bistrot de Zinal</h1> <p>Si vous voyez ce message, l'application fonctionne correctement !</p> </div> ); }
