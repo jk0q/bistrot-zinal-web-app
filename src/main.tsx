ff26b050f07a4b8010e0d667fec5b923bd89ebf5
jk0q <kevinferrepetersen@gmail.com>
2025-03-12 19:22:52 +0100

    Correction du fichier main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { MantineProvider } from '@mantine/core'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import '@mantine/core/styles.css'
+import './styles/global.css'
 import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <MantineProvider>
+      <BrowserRouter basename="/bistrot-zinal-web-app">
+        <App />
+      </BrowserRouter>
+    </MantineProvider>
+  </React.StrictMode>,
 )
